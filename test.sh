a64fa92024f93f1806a8dfdd75318428ed4f5549
ewyand <erikwyand@priserv.com>
2017-02-09 18:03:54 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,14 +10,14 @@ apt-get update && apt-get upgrade -y --force-yes && apt-get install -y --force-y
 sed "s@echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main' >> /etc/apt/sources.list.d/pgdg.list@#echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main' >> /etc/apt/sources.list.d/pgdg.list@g" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
 sed "s@wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -@#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -@g" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
 sed "s/\<apt-get update && apt-get upgrade -y\>/#nope/" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
-sed "s@apt-get install -y --force-yes sudo postgresql@#nope@" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
+sed "s@\<apt-get install -y --force-yes sudo postgresql\>@#nope@" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
 
 sed "s@#echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main'  >> /etc/apt/sources.list.d/postgresql.list@echo 'deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main'  >> /etc/apt/sources.list.d/postgresql.list@g" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
 sed "s@#echo 'deb http://packages.2ndquadrant.com/bdr/apt/ jessie-2ndquadrant main' >> /etc/apt/sources.list.d/2ndquadrant.list@echo 'deb http://packages.2ndquadrant.com/bdr/apt/ jessie-2ndquadrant main' >> /etc/apt/sources.list.d/2ndquadrant.list@g" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
 sed "s@#/usr/bin/wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -@/usr/bin/wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -@g" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
 sed "s@#/usr/bin/wget --quiet -O - http://packages.2ndquadrant.com/bdr/apt/AA7A6805.asc | apt-key add -@/usr/bin/wget --quiet -O - http://packages.2ndquadrant.com/bdr/apt/AA7A6805.asc | apt-key add -@g" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
-sed "s@#apt-get update && apt-get upgrade -y@apt-get update && apt-get upgrade -y@" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
-sed "s@#apt-get install -y --force-yes sudo postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4@apt-get install -y --force-yes sudo postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4@" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
+sed "s@\<#apt-get update && apt-get upgrade -y\>@apt-get update && apt-get upgrade -y@" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
+sed "s@\<#apt-get install -y --force-yes sudo postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4\>@apt-get install -y --force-yes sudo postgresql-bdr-9.4 postgresql-bdr-9.4-bdr-plugin postgresql-bdr-contrib-9.4@" -i /usr/src/fusionpbx-install.sh/debian/resources/postgres.sh
 
 
 cd /usr/src/fusionpbx-install.sh/debian && nano resources/postgres.sh
