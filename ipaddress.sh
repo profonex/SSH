87e4086073340c5761eea05682122039b934764d
ewyand <erikwyand@priserv.com>
2017-02-09 16:35:33 -0500

    Create ipaddress.sh

---
+++ ipaddress.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+read -p 'IP Address: ' ip_address
+read -p 'Subnet Mask: ' mask
+read -p 'Gateway: ' gateway
+
+sed "s@iface eth0 inet dhcp@iface eth0 inet static@g" -i /etc/network/interfaces
+
+cat >> /etc/network/interfaces << EOF
+address $ip-address
+netmask $mask
+gateway $gateway
+EOF
+
+sed "s@127.0.1.1@127.0.0.1@g" -i /etc/hosts
+
+cat > /etc/resolv.conf < EOF
+nameserver 4.2.2.2
+nameserver 8.8.8.8
+EOF
+
+
+service networking restart
+
