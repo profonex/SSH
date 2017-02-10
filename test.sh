9dc7430c4f52174207614782934f02e6af1da961
ewyand <erikwyand@priserv.com>
2017-02-10 01:49:07 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@ read -p "Total Number of Nodes: " tnode
 
 max=$tnode
 
-for i in 'seq $max'
+for i in `seq $max`
 do
-  read -p "Node $i IP Address: " ip
-  echo "$i $ip"
+    read -p "Node $i IP Address: " ipadd
+    nodenum=$i
+    ip=$ipadd
+    nodeip=
+    echo "$i$ipnode" "$ip" "$i"
 done
-
