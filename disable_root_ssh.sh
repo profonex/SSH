cb437242d85ddb264bea6c30a435f88b96ddb768
ewyand <erikwyand@priserv.com>
2017-02-14 00:32:07 -0500

    Update disable_root_ssh.sh

--- disable_root_ssh.sh
+++ disable_root_ssh.sh
@@ -3,4 +3,5 @@
 sed "s@PermitRootLogin yes@PermitRootLogin without-password@g" -i /etc/ssh/sshd_config
 sed "s@#PasswordAuthentication yes@PasswordAuthentication no@g" -i /etc/ssh/sshd_config
 
-service ssh restart
+service ssh stop
+service ssh start
