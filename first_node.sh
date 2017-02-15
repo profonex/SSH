963e59577abdd94808b3b986c416c874560efa95
ewyand <erikwyand@priserv.com>
2017-02-15 16:35:09 -0500

    Update first_node.sh

--- first_node.sh
+++ first_node.sh
@@ -92,6 +92,8 @@ done
 systemctl daemon-reload
 systemctl restart postgresql
 
+export PGPASSWORD=$dbasepass
+
 sudo -u postgres psql -c "ALTER USER fusionpbx WITH PASSWORD '$dbasepass';"
 sudo -u postgres psql -c "ALTER USER freeswitch WITH PASSWORD '$dbasepass';"
 sudo -u postgres psql -d fusionpbx -c "CREATE EXTENSION btree_gist;"
@@ -104,6 +106,7 @@ sudo -u postgres psql -d fusionpbx -c "SELECT bdr.bdr_node_join_wait_for_ready()
 sudo -u postgres psql -d freeswitch -c "SELECT bdr.bdr_group_create(local_node_name := '$nodename', node_external_dsn := 'host=$thisip port=5432 dbname=freeswitch connect_timeout=10 keepalives_idle=5 keepalives_interval=1 sslmode=require');"
 sudo -u postgres psql -d freeswitch -c "SELECT bdr.bdr_node_join_wait_for_ready();"
 #sudo -u postgres psql -d freeswitch -c "CREATE EXTENSION pgcrypto;"
+
 
 #add the config.php
 rm -R /etc/fusionpbx
