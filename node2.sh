a898d68cb80158ae4318eee75fed870f79aef03a
ewyand <erikwyand@priserv.com>
2017-02-23 10:10:45 -0500

    Update node2.sh

--- node2.sh
+++ node2.sh
@@ -194,8 +194,8 @@ wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-ke
 apt-get update
 apt-get install resilio-sync
 
-sed -i '8,9s/btsync/www-data/' /lib/systemd/system/resilio-sync.service
-sed -i '15s/btsync:btsync/www-data:www-data/' /lib/systemd/system/resilio-sync.service
+sed -i '8,9s/rslsync/www-data/' /lib/systemd/system/resilio-sync.service
+sed -i '15s/rslsync:rslync/www-data:www-data/' /lib/systemd/system/resilio-sync.service
 
 chown -R www-data:www-data /var/lib/resilio-sync
 systemctl daemon-reload
