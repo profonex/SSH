f2adf28b4ae207a717126e3a393b9a384eaf695c
ewyand <erikwyand@priserv.com>
2017-02-23 10:56:24 -0500

    Update btsync_upgrade.sh

--- btsync_upgrade.sh
+++ btsync_upgrade.sh
@@ -3,7 +3,7 @@
 
 systemctl stop btsync
 
-sh -c 'echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list'
+sh -c 'echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" > /etc/apt/sources.list.d/resilio-sync.list'
 wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | apt-key add -
 apt-get update
 apt-get install -y resilio-sync
