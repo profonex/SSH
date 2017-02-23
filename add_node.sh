68580c7d30c32f7d3c15b7c7e7013743c34558d5
ewyand <erikwyand@priserv.com>
2017-02-23 11:31:44 -0500

    Update add_node.sh

--- add_node.sh
+++ add_node.sh
@@ -187,18 +187,21 @@ mkdir -p /etc/fusionpbx/resources/templates/
 chown -R www-data:www-data /etc/fusionpbx
 
 
-sh -c 'echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list'
-wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+sh -c 'echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" > /etc/apt/sources.list.d/resilio-sync.list'
+wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | apt-key add -
 apt-get update
-apt-get install resilio-sync
+apt-get install -y resilio-sync
 
-sed -i '8,9s/btsync/www-data/' /lib/systemd/system/resilio-sync.service
-sed -i '15s/btsync:btsync/www-data:www-data/' /lib/systemd/system/resilio-sync.service
+sed -i '8,9s/rslsync/www-data/' /lib/systemd/system/resilio-sync.service
+sed -i '15s/rslsync:rslsync/www-data:www-data/' /lib/systemd/system/resilio-sync.service
 
 chown -R www-data:www-data /var/lib/resilio-sync
 systemctl daemon-reload
+systemctl stop resilio-sync
+rslsync --config rslsync/rslsync.conf
 systemctl restart resilio-sync
-ystemctl enable resilio-sync
+systemctl enable resilio-sync
+
 
 #remove previous install
 rm -R /opt/letsencrypt
