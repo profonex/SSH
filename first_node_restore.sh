c0aac4f98340d9daa31719bb14be42471be92edf
ewyand <erikwyand@priserv.com>
2017-12-15 00:56:00 -0500

    Update first_node_restore.sh

--- first_node_restore.sh
+++ first_node_restore.sh
@@ -19,7 +19,7 @@ do
     c=$((c+1));
 done
 
-read -p "What is the FQDN of this Node: " domainname
+#read -p "What is the FQDN of this Node: " domainname
 #read -p "Username for this Node: " username
 #read -p "Password for this Node: " userpass
 #read -p "What is your email address: " email_address
